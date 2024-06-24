5c1c31e13b5472f55dc6235c5d789aa768b3498a
Dhamodharan Ravichandran <162108469+Dhamu2303@users.noreply.github.com>
2024-06-24 11:19:50 +0530

    Add files via upload

---
+++ StudentRepoandServices/src/main/java/com/example/StudentRepoandServices/StudentRepoandServicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentRepoandServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRepoandServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRepoandServicesApplication.class, args);
+	}
+
+}
---
+++ StudentRepoandServices/src/test/java/com/example/StudentRepoandServices/StudentRepoandServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentRepoandServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRepoandServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
